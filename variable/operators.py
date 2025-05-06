af0a25a0be6fa5927fc4f247c5cc98d8324dbcc5
jyotiteena <jingarjyoti13@gmail.com>
2025-05-06 14:36:21 +0530

    operators

---
+++ variable/operators.py
@@ -0,0 +1,33 @@
+# arithmetic operator
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2) # 2.5
+print(4/2) #2.0
+print(5%2)
+print(4**2) # power
+
+
+# comparison or relational 
+print(4>3) # True
+print(4>=3) # True
+print(4<3) # False
+print(4<=3)
+print(4==3)
+print(4!=3)
+
+# assignment operator
+# =, +=, **= others all
+
+# logical operator
+# not, and , or 
+
+print(not False)
+print(not True)
+
+a = 34
+b = 2
+print(a>b and a==b)
+print(a>b or a==b)
+print((a>b) or (a==b))
+print(not(a>b and a==b)) # True
