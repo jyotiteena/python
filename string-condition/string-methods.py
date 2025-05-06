80a0036b802f65d3593b5b68322b34363990d986
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-06 23:54:40 +0530

    string methods

--- string-condition/string-methods.py
+++ string-condition/string-methods.py
@@ -1,2 +1,18 @@
-course = "web development"
-print(course.endswith("nt"))
+course = "web development web to web"
+print(course.endswith("nt")) # True
+print(course.endswith("nts")) # False
+# print(course.capitalize())  # not reflect direct original string
+course = course.capitalize() 
+print(course)
+print(course.replace("web","Website")) #Web development Website to Website
+# find(), count()
+
+print(course.find("dev")) # 4
+print(course.count("web")) #2
+print(course.count("Web")) # 1
+
+
+# / check $ symobol how many time occurance from string 
+
+dollar = "my price is $900 and other is $900"
+print(dollar.count("$")) #2
