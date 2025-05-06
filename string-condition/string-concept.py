6b819ba61d54fe02a34b4370fb1d52c97f2ae3c5
jyotiteena <jingarjyoti13@gmail.com>
2025-05-06 16:02:16 +0530

    concat and length

---
+++ string-condition/string-concept.py
@@ -0,0 +1,34 @@
+str1 = "hello"
+str2  = 'world'
+
+# its like pre tag
+str3 = """
+first
+      second
+"""
+str4 = '''
+    4th and 
+    5th
+'''
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+
+str5 = "it's a good person"
+print(str5)
+
+str6 = 'it"s a good person'
+print(str6) #it"s a good person
+
+str7 = "this is first para \n and second para is this"
+print(str7)
+
+str8 = "this is first para \t and second para is this"
+print(str8)
+
+
+# concat 
+print(str1+ " " +str2)
+
+print(len(str1))
