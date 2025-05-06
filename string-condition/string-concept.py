02bfcbdae487d275cd07bdafcbd118eef8278533
jyotiteena <jingarjyoti13@gmail.com>
2025-05-06 20:31:15 +0530

    string slicing

--- string-condition/string-concept.py
+++ string-condition/string-concept.py
@@ -27,8 +27,32 @@ print(str7)
 str8 = "this is first para \t and second para is this"
 print(str8)
 
-
 # concat 
 print(str1+ " " +str2)
 
 print(len(str1))
+print(str1[0])
+print(str1[1])
+
+# /// can't change character override or can't manupulate
+# str1[0] = "s" error
+# print(str1)
+
+
+# most of methods use  in AI/ML
+#  slicing 
+
+# str[start_index:end_index ] --> end_index(ending index not included )
+
+name = "jyoti jingar"
+print(name[0:3]) # jyo
+print(name[:3]) # jyo
+print(name[1:])  #yoti jingar
+print(name[1:len(name)])  #yoti jingar
+print(name[6:13]) #jingar
+
+
+# //// -ve index 
+# jyoti -> j(-5), i(-1)
+# -1 not included 
+print(name[-3:-1]) #ga
