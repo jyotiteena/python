d8e88be670aac4e37618fa6f0bdacc2937105f0b
jyotiteena <jingarjyoti13@gmail.com>
2025-05-06 14:52:05 +0530

    type casting

---
+++ variable/type-conversion.py
@@ -0,0 +1,23 @@
+# type of conversion 
+# 1. type conversion ( automatically)
+# 2. type casting  (manually)  , int(), str(), float()
+
+
+a = 34
+b = 3
+sum = a+b
+print(sum)
+
+c = "4"
+d = 5
+add = int(c)+d
+print(add)
+
+str2 = "jyoti"
+e = 3
+
+e = str(e)
+
+print(type(e))
+
+# addition = int(str2) + 3 error
