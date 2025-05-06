20c058fec81c943fc5f89005785f67003d677d52
jyotiteena <jingarjyoti13@gmail.com>
2025-05-06 15:16:11 +0530

    input concept by default string

---
+++ input-method.py
@@ -0,0 +1,19 @@
+# print("enter first name")
+# input()
+
+# input("enter second name = ")
+
+# output = input("enter third value = ")
+# print("output = ",output)
+
+# by defaultdata type - string 
+
+a = input("enter a value :")
+b = input("enter b value : ")
+sum = a +b
+print(a+b)  #1222
+
+val1 = int(input("enter val1 value :"))
+val2 = int(input("enter val2 value : "))
+add = val1 + val2
+print('add: ', add)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+# input()
+
+# input("enter second name = ")
+
+# output = input("enter third value = ")
+# print("output = ",output)
