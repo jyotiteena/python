4468284704ec7a1f42a2093cd37470edcf35f5fd
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-07 23:13:39 +0530

    start list

---
+++ list.py
@@ -0,0 +1,16 @@
+# different data type values store 
+
+list1 = [12,22.3,True,"jyoti"]
+print(list1)
+print(type(list1)) #<class 'list'>
+print(len(list1))
+
+# string is immutabe
+# list is mutable
+
+str1 = "jyoti"
+# str1[0] = "t" #TypeError: 'str' object does not support item assignment
+print(list1)
+list1[2] = "second"
+print(list1)
+# print(list1[7]) #IndexError: list index out of range
