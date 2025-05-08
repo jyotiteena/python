55927d808a602e672fc43f16efd9c4bfba433743
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-08 21:00:40 +0530

    changes

--- list.py
+++ list.py
@@ -10,7 +10,7 @@ print(len(list1))
 
 str1 = "jyoti"
 # str1[0] = "t" #TypeError: 'str' object does not support item assignment
-print(list1)
+  print(list1)
 list1[2] = "second"
 print(list1)
 # print(list1[7]) #IndexError: list index out of range
