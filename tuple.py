e45930203bb8c44e5a05e24ed605f905ead85d99
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-08 21:43:57 +0530

    tuple concept

---
+++ tuple.py
@@ -0,0 +1,21 @@
+# immutable
+tuple1 = (12,3,4.5)
+print(tuple1[0])
+print(tuple1[1])
+print(tuple1[2])
+
+# tuple1[1] = 11
+# print(tuple1) TypeError: 'tuple' object does not support item assignment
+
+empTuple = ()
+print(empTuple) #()
+
+print(type(empTuple)) #<class 'tuple'>
+
+tuple3 = (1)
+print(type(tuple3)) #<class 'int'>
+
+tuple4 = (1,)
+print(type(tuple4)) #<class 'tuple'>
+
+# single , (comma) pass compulsory 
