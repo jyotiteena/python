987faf1811b847246523112a2d6608e76667d35f
jyotiteena <jingarjyoti13@gmail.com>
2025-05-09 16:00:47 +0530

    about the set

---
+++ set.py
@@ -0,0 +1,16 @@
+# set is collection of unordered items and must be unique and immutable
+
+# list and dict can't store into set bcoz of both are mutable
+
+num = {1,2,3,4,2}
+print(num) #{1, 2, 3, 4}
+
+# how to write empty set 
+collection = {}
+print(collection)
+print(type(collection)) # this i dictionary 
+
+set1 = set()
+print(set1) #set()
+print(type(set1)) #<class 'set'>
+
