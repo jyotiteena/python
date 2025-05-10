f41375eea89b88b92ee4e5da367ba0f8c4150645
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-11 00:20:14 +0530

    simple function

---
+++ functions/type-of-function.py
@@ -0,0 +1,10 @@
+# two type of function 
+# built-in fn -> print(), len(), type(), range()
+# user define fn 
+
+print("first", end=" / ")
+print("second")
+
+print("third", "forth", sep=", ")
+
+# default parameter 
