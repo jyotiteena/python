7eb8bafbb722f159cf2c39ce7b28f5c97ec8b740
jyotiteena <jingarjyoti13@gmail.com>
2025-05-12 15:41:05 +0530

    del keyword

---
+++ oops2/delete.py
@@ -0,0 +1,11 @@
+# delete object and object properties
+
+class Student:
+    def __init__(self):
+        print("delete concept")
+        
+s1 = Student()
+print(s1)
+
+del s1
+print(s1) #name 's1' is not defined
