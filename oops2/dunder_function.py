c737fd02a257c9b6b946a97a67eff6121999ab01
jyotiteena <jingarjyoti13@gmail.com>
2025-05-13 15:06:53 +0530

    all dunder functions

--- oops2/dunder_function.py
+++ oops2/dunder_function.py
@@ -1,4 +1,11 @@
 # __ (double score)  called dunder function 
+# 1. __add__
+# 2. __sub__
+# 3. __mul__
+# 4. __truediv__
+# 5. __mod__
+# 6. __init__ 
+# 7. __gt__ 
 
 class Complex:
     def __init__(self,real,img):
@@ -11,6 +18,11 @@ class Complex:
     def add(self,num2):
         new_real = self.real + num2.real
         new_img = self.img + num2.img
+        return Complex(new_real,new_img) 
+       
+    def __add__(self,num2):
+        new_real = self.real + num2.real
+        new_img = self.img + num2.img
         return Complex(new_real,new_img)    
     
 c1 = Complex(12,2)
@@ -20,3 +32,6 @@ c2.show()
 result = c1.add(c2)
 result.show()
     
+# i want to like this num1 + num2 , its possible by dunder function
+c3 = c1 + c2
+c3.show()
