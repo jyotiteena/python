0a030392a55b793c9128d92509a30e3a11b02457
jyotiteena <jingarjyoti13@gmail.com>
2025-05-13 13:12:54 +0530

    property decorator

--- oops2/peroperty-decorator.py
+++ oops2/peroperty-decorator.py
@@ -3,10 +3,15 @@ class Student:
         self.hindi =  hindi
         self.eng =  eng
         self.maths =  maths
-        self.per = str((self.hindi+self.eng+self.maths)/len([self.hindi,self.eng,self.maths])) + "%"
+        # self.per = str((self.hindi+self.eng+self.maths)/len([self.hindi,self.eng,self.maths])) + "%"
+        
+    @property
+    def perMark(self):
+        return str((self.hindi+self.eng+self.maths)/len([self.hindi,self.eng,self.maths])) + "%"
         
 std1 = Student(90,80,70)
-print("per = ",std1.per)
+print(std1.perMark)
+# print("per = ",std1.per)
 std1.eng = 90
-print(std1.eng)
-print("per = ",std1.per) # can not change per according to std1.end = 90
+# print("per = ",std1.per) # can not change per according to std1.end = 90
+print(std1.perMark)
