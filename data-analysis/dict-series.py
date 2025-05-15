89cd7cecdcbc865a13a8018b65ed542e1ebfe9f2
jyotiteena <jingarjyoti13@gmail.com>
2025-05-15 15:19:00 +0530

    complete series with join

---
+++ data-analysis/dict-series.py
@@ -0,0 +1,41 @@
+import pandas as pd
+dict = {"name":["jyoti","teena"],"rank":[12,2,3]}
+output = pd.Series(dict)
+print(output)
+
+# //////// answer ////////
+# name    [jyoti, teena]
+# rank        [12, 2, 3]
+# dtype: object - because of mix data
+
+
+# single data also can be added
+
+output2 = pd.Series(12);
+print(output2)
+
+#0    12
+
+
+# same data on all index 
+
+output3 = pd.Series(12, index=[1,2,3,4])
+print(output3)
+
+
+# ///////// answer ///////
+# 0    12
+# 1    12
+# 2    12
+# 3    12
+# 4    12
+# dtype: int64
+output4 = pd.Series(11,[1,2])
+print(output3 + output4)
+# 1    23.0
+# 2    23.0
+# 3     NaN
+# 4     NaN
+# dtype: float64
+
+
