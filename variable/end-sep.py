56dad2014eead7d777ab15848bcf2b4dc19c48bd
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-30 15:35:38 +0530

    end and sep parameter

---
+++ variable/end-sep.py
@@ -0,0 +1,24 @@
+print("jyoti",end=" ")
+print("jingar", end=", ")
+print("Full stack developer")
+
+date = 30
+month = 5
+year = 2025
+print(date,month,year,sep="/",end=" thank you")
+print()
+print(date,month,year,end=" thank you",sep="/")
+
+print()
+
+for i in range(1,6):
+    print("value",i,sep=":",end=" -> ")
+    
+print()
+for i in range(1, 6):
+    print(f"value:{i} = {i*2}", end=" | " if i < 5 else "")
+    # value:1 = 2 | value:2 = 4 | value:3 = 6 | value:4 = 8 | value:5 = 10
+
+    
+    
+    
