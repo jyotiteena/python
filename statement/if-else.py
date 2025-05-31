ca5ffee05ae3630f770d236df3c428d313c52801
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-31 17:55:59 +0530

    switch alternate match

---
+++ statement/if-else.py
@@ -0,0 +1,5 @@
+age = 19
+if age>18: print("you are eligible for voting")
+else : print("you are not eligible for voting")
+
+print("you are eligible for voting") if age>20 else print("you are not eligible for voting")
---
+++ statement/match-case.py
@@ -0,0 +1,10 @@
+choice = 5
+match choice:
+    case 1 :
+        print("your choice is 1")
+    case 2:
+        print("your choice is 2")
+    case 3|4:
+        print("your choice is 3 or 4")
+    case _:
+        print("wrong choice")
