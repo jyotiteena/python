3790a75b5ba7117caef3f8e9e3489e7b4ff74592
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-05-31 17:11:41 +0530

    ternary operator

---
+++ variable/ternary.py
@@ -0,0 +1,7 @@
+a = 10
+if a>10:
+    print("a is greater than 10 value")
+else :
+    print("a is not agreater than 10 value")
+
+print("a is greater than 10 value") if a>10 else  print("a is not agreater than 10 value")
