a40e0da9bf43964195afa2a9d95a7c608b8f630a
jyotiteena <jingarjyoti9413@gmail.com>
2025-06-15 10:26:51 +0530

    string formatting

---
+++ string-condition/string-formatting.py
@@ -0,0 +1,13 @@
+a = 10
+b = 4
+
+
+# c-style 
+print("addition of %d and %d = %d" %(a,b,a+b))
+
+# format method 
+
+print("addition of {} and {} = {}".format(a,b,a+b))
+
+# f-string 
+print(f"addition of {a} and {b} = {a+b}")
