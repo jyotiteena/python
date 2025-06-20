d2688d53aed06011fdb500b0fac610efd335fe49
jyotiteena <jingarjyoti9413@gmail.com>
2025-06-20 20:32:26 +0530

    dict methods

--- list-methods/dict-method.py
+++ list-methods/dict-method.py
@@ -1,6 +1,21 @@
 dict1 = {"name":"jyoti",'grid':2333}
 print(dict1)
 print(dict1.values())
+
 print(dict1.keys())
+
 print(dict1.items()) #[('name', 'jyoti'), ('grid', 2333)])
-print(dict1.fromkeys('name'))
+
+print(dict1.get('name')) # jyoti
+
+print(dict1.get('phone')) # None
+dict1.pop('grid')
+
+print(dict1)
+
+dict1.update({'age':34,'grid':322}) # add key value
+
+print(dict1)
+
+dict1.popitem() # remove last key
+print(dict1) #{'name': 'jyoti', 'age': 34}
