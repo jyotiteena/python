7d4fac90b104d1778a6333911fe5fef2dc37f671
jyotiteena <jingarjyoti9413@gmail.com>
2025-06-23 22:52:56 +0530

    lambda

--- lambda/intro.py
+++ lambda/intro.py
@@ -10,3 +10,13 @@ print(square)
 
 filterData = list(filter(lambda ele: ele>2,list1))
 print(filterData)
+
+list2 = [{"id":123,"name":"jyoti"},{"id":232,"name":"teena"}]
+show = map(lambda ele:ele, list2)
+show2 = list(map(lambda ele:ele['name'], list2))
+print("show = ",show)
+print("show2 = ",show2) #['jyoti', 'teena']
+
+filterData = list(filter(lambda ele: ele['id']==123, list2))
+print(filterData)
+
