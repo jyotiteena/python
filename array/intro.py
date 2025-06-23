c876bfc4ceb32c21f3f021e4984d02413ad37829
jyotiteena <jingarjyoti9413@gmail.com>
2025-06-23 23:28:01 +0530

    numpy

---
+++ array/intro.py
@@ -0,0 +1,3 @@
+import array
+arr = array.array('i',[44,3,2,27,23])
+print(arr)
---
+++ array/numpy2.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr = np.array([44,3,2,27,23])
+print(arr)
+
+arr.sort()
+print(arr)
+
+arr2 = np.array([100,44,22,4])
+print(arr2)
+sortedArr = sorted(arr2)
+print(arr2)
+print(sortedArr)
