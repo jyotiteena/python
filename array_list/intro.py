d1695499683dd0616c85027f8c1aa23b976ad1c7
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-06-26 12:19:20 +0530

    sorting with number using sorted method

---
+++ array_list/intro.py
@@ -0,0 +1,13 @@
+arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print("2D Array:")
+for row in arr:
+    print(row)
+
+print("arr[0]...............")
+print(arr[0])
+print(arr[0][0])
---
+++ array_list/sort-sorted.py
@@ -0,0 +1,9 @@
+list1  = [34,5,6,7]
+
+new_list = sorted(list1)
+print(list1)
+print(new_list)
+
+
+new_list = sorted(list1,reverse=True)
+print("reverse = ",new_list)
