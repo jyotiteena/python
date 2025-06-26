96d77067f4d5ebec0bb031ba9e0a9c236cfd9066
jyotirwa3 <rwa3.jyoti.sj@gmail.com>
2025-06-26 12:21:29 +0530

    sorting with string using sorted

--- array_list/sort-sorted.py
+++ array_list/sort-sorted.py
@@ -7,3 +7,13 @@ print(new_list)
 
 new_list = sorted(list1,reverse=True)
 print("reverse = ",new_list)
+
+
+
+# ///////// string sorting //////////
+words = ["teena","mega","jyoti"]
+new_words = sorted(words,key=len)
+print(new_words)
+
+new_words_rev = sorted(words,reverse=True, key=len)
+print(new_words_rev)
